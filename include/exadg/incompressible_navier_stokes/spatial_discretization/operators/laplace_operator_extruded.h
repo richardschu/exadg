13cfebd7ff15ad5990c3996cd440aec71fbeffe7
Martin Kronbichler <martin.kronbichler@rub.de>
2026-05-26 22:20:31 +0200

    Remove debug output. Disable manufactured solution by default.

--- include/exadg/incompressible_navier_stokes/spatial_discretization/operators/laplace_operator_extruded.h
+++ include/exadg/incompressible_navier_stokes/spatial_discretization/operators/laplace_operator_extruded.h
@@ -2011,6 +2011,9 @@ private:
       AssertThrow(index != numbers::invalid_unsigned_int, ExcInternalError());
     for(const auto & index : touched_last_by)
       AssertThrow(index != numbers::invalid_unsigned_int, ExcInternalError());
+
+    // debug output to assess sanity of implementation
+    if(false)
     {
       unsigned int n_batches_half = 0, n_batches_10 = 0;
       for(unsigned int i = 0; i < touched_first_by.size(); ++i)
@@ -2050,6 +2053,8 @@ private:
     std::cout << std::endl;
 #endif
 
+    // debug output to assess sanity of implementation
+    if(false)
     {
       unsigned int n_batches_half = 0, n_batches_10 = 0;
       for(unsigned int i = 0; i < touched_first_by.size(); ++i)
@@ -3313,9 +3318,11 @@ private:
         else
           ++count_1;
 
-    Helper::print_time(static_cast<double>(count_1) / (count_0 + count_1),
-                       "Face flux buffer efficiency",
-                       MPI_COMM_WORLD);
+    // debug output to assess sanity of implementation
+    if(false)
+      Helper::print_time(static_cast<double>(count_1) / (count_0 + count_1),
+                         "Face flux buffer efficiency",
+                         MPI_COMM_WORLD);
 
 #if 0
     std::cout << "Identified storage of length " << size_of_storage << " with "
